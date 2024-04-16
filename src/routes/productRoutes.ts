2b8fcaf13ebb30b5b7c095069a82641628e194c4
Jake Roberts <jdr81394@gmail.com>
2024-04-16 10:00:22 +0200

    product routes are connected to data lake

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -1,29 +1,43 @@
 import express, { Request, Response, NextFunction } from "express";
 import dotenv from "dotenv";
-import Product from "../models/ProductModel";
 import User from "../models/UserModel";
 import HttpError from "../models/http-errors";
-import { ObjectId } from "mongodb";
 
 dotenv.config();
 
 const router = express.Router();
+const dataLakeApiKey = process.env.DATA_LAKE_API_KEY
 const dataLakeUri = process.env.SERVER_DATA_LAKE_URI_LOCAL;
 
 //GET ALL PRODUCT
+// Connecté à datalake
 //@GET
 //api/v1/product
 router.get("/", async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const products = await Product.find();
+    // const products = await Product.find();
+    const response = await fetch(dataLakeUri + "/reference", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "app-id": `${dataLakeApiKey}`
+      }    
+    });
 
-    res.status(201).json({ products: products });
+    if(response.status !== 200 ) {
+      throw new HttpError("GET tous les produit n'a pas bien functioné ",400);
+    }
+
+    const products = await response.json();
+
+    res.status(201).json({ products });
   } catch (err) {
     res.status(500).json({ error: { message: "un probleme est survenu" } });
   }
 });
 
 //GET PRODUCT BY ID
+// Connecté à datalake
 //@GET
 //api/v1/product/:id
 router.get("/:id", async (req: Request, res: Response, next: NextFunction) => {
@@ -31,7 +45,19 @@ router.get("/:id", async (req: Request, res: Response, next: NextFunction) => {
 
   let product;
   try {
-    product = await Product.findById(id);
+    const response = await fetch(dataLakeUri + "/reference/" + id, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "app-id": `${dataLakeApiKey}`
+      }    
+    });
+    if(response) {
+      product = await response.json();
+    } else {
+      throw new HttpError("Un problème à propos de la creation de la reference s'est passé", 400);
+    }
+
   } catch (err) {
     const error = new HttpError(
       "Un problème est survenu, impossible de trouver l'utilisateur",
@@ -48,10 +74,11 @@ router.get("/:id", async (req: Request, res: Response, next: NextFunction) => {
     return next(error);
   }
 
-  res.json({ product: product.toObject({ getters: true }) });
+  res.json({ product });
 });
 
 //CREATE
+// Connecté à datalake
 //@POST
 //api/v1/product/create
 router.post(
@@ -79,7 +106,7 @@ router.post(
             method: "POST",
             headers: {
               "Content-Type": "application/json",
-              "app-id": "password"
+              "app-id": `${dataLakeApiKey}`
             }, 
             body: JSON.stringify(uvc)
           });
@@ -98,7 +125,7 @@ router.post(
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          "app-id": "password"
+          "app-id": `${dataLakeApiKey}`
         },
         body: JSON.stringify(referenceBody)
       });
@@ -137,7 +164,7 @@ router.post(
         // delete reference
         fetch(dataLakeUri + "/reference/" + product._id, { method: "DELETE", headers: {
           "Content-Type": "application/json",
-          "app-id": "password"
+          "app-id": `${dataLakeApiKey}`
         }})
         throw new HttpError("Erreur a propos de updated user: "+ updatedUser , 400);
 
@@ -156,6 +183,7 @@ router.post(
 );
 
 //UPDATE
+// Connecté à datalake
 //@PATCH
 //api/v1/product/create
 router.patch(
@@ -175,9 +203,17 @@ router.patch(
 
     try {
       // Vérifier si le produit existe
-      let product = await Product.findById(id);
-      if (!product) {
-        throw new HttpError("Produit non trouvé.", 404);
+      // let product = await Product.findById(id);
+      const productResponse = await fetch(dataLakeUri + "/reference/" + id, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "app-id": `${dataLakeApiKey}`
+        }    
+      });
+
+      if (!productResponse) {
+        throw new HttpError("La rèquete pour le produit ne s'est pas bien passé.", 404);
       }
 
       // Vérifier si l'utilisateur existe
@@ -186,6 +222,9 @@ router.patch(
         throw new HttpError("Utilisateur non trouvé.", 404);
       }
 
+      
+      const product = await productResponse.json();
+      const oldProduct = product;   // il peut etre utilisé en cas de défaire
       // Mettre à jour les détails du produit
       product.reference = reference;
       product.name = name;
@@ -194,40 +233,67 @@ router.patch(
       product.productCollection = productCollection;
       product.imgPath = imgPath;
 
+      if(product.creator === undefined ) product.creator  = []
+
+      // console.log("PRODUCT: " , product)
       product.creator.push({
         _id: user._id,
         username: user.username,
         email: user.email,
       });
 
+      const putResponse = await fetch(dataLakeUri + "/reference", {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          "app-id": `${dataLakeApiKey}`
+        },
+        body: JSON.stringify(product)    
+      });
+
       // Enregistrer les modifications du produit
-      const updatedProduct = await product.save();
+      // const updatedProduct = await product.save();
+
+      if(putResponse.status !== 200) {
+        throw new HttpError("Erreur de put sur le produit", 400)
+      }
 
       // Mettre à jour le champ products de l'utilisateur
-      const updatedUser = await User.findByIdAndUpdate(
+      const updatedUser : Document | null | undefined = await User.findByIdAndUpdate(
         creator,
         {
           $push: {
             products: {
-              _id: updatedProduct._id,
-              reference: updatedProduct.reference,
-              name: updatedProduct.name,
-              date: updatedProduct.updatedAt,
+              _id: product._id,
+              reference: product.reference,
+              name: product.name,
+              date: product.updatedAt,
             },
           },
         },
         { new: true }
-      ).populate("products");
+      )
+      
+      // Mettre à jour l'utilisateur
+      if(updatedUser === undefined || updatedUser === null) {
+        // Défaire la rèquete vers data lake
+        fetch(dataLakeUri + "/reference", {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+            "app-id": "password"
+          },
+          body: JSON.stringify(oldProduct)    
+        });
+  
+        throw new HttpError("L'utilisateur ne pouvait pas etre mis à jour", 400);
+      }
 
       res
         .status(200)
-        .json({ product: updatedProduct.toObject({ getters: true }) });
+        .json(product );
     } catch (err) {
-      const error = new HttpError(
-        "Échec lors de la mise à jour du produit, réessayez plus tard.",
-        500
-      );
-      return next(error);
+      return next(err);
     }
   }
 );
@@ -235,18 +301,31 @@ router.patch(
 //DELETE PRODUCT
 //@DELETE
 //api/v1/product/delete/:id
-router.delete(
-  "/delete/:id",
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const productId = req.params.id;
-      await Product.findByIdAndDelete(productId);
+// Pas de CRUD o: que le CRU ;)
+// router.delete(
+//   "/delete/:id",
+//   async (req: Request, res: Response, next: NextFunction) => {
+//     try {
+//       const productId = req.params.id;
+//       // await Product.findByIdAndDelete(productId);
+//       const response = await fetch(dataLakeUri + "/reference/" + productId, {
+//         method: "DELETE",
+//         headers: {
+//           "Content-Type": "application/json",
+//           "app-id": "password"
+//         }    
+//       });
 
-      res.status(200).json({ message: "Produit supprimé avec succès" });
-    } catch (error) {
-      res.status(500).json({ message: "Erreur lors de la suppression" });
-    }
-  }
-);
+//       if(response.status !== 200) {
+//         throw new Error("Produit n'etait pas supprimé")
+//       } 
+
+//       res.status(200).json({ message: "Produit supprimé avec succès" });
+//     } catch (error) {
+//       console.error(error)
+//       res.status(500).json({ message: "Erreur lors de la suppression" });
+//     }
+//   }
+// );
 
 export default router;
