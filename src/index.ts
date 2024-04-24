e68a8696c9fea79804826674db9a5dda9a510373
Vincent Coccina <v.coccina@acs2i.fr>
2024-04-24 08:44:28 +0200

    fix

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:3002",
+    origin: "http://localhost:3000",
     methods: "GET,POST,PUT,DELETE,PATCH",
     credentials: true,
   })
