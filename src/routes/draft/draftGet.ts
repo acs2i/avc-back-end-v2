38b6ccc2d9bf0e61a79569bfee0529859d783aac
Jake Roberts <jdr81394@gmail.com>
2024-05-13 17:31:51 +0200

    draft get now exists and it gets alld rafts that belong to the users id, and it accepts 3 query parameters for the draft itself: code article, ga libcompl and ga libelle

--- src/routes/draft/draftGet.ts
+++ src/routes/draft/draftGet.ts
@@ -2,8 +2,60 @@ import { verifyToken } from './../../middleware/auth';
 import express, { Request, Response } from "express"
 import { DRAFT } from "./shared";
 import DraftModel from '../../models/draftSchema';
+import { generalLimits } from '../../services/generalServices';
 
 const router = express.Router();
+
+
+// This will only get all the drafts that belong to a specific user
+// This will also have search functionalities as well too...? right practice? perhaps. 
+router.get(DRAFT, verifyToken, async (req: Request & { user?: {id: string}}, res: Response) => {
+    try {
+        
+        if(!req.user) {
+            throw new Error("User was not authenticated");
+        }
+
+        const {skip, intLimit} = await generalLimits(req);
+
+        const CREATOR_ID = req.user.id;
+        
+        if(!CREATOR_ID) {
+            throw new Error("Somehow creator id was falsy")
+        }
+
+        const filter = { $and: [{CREATOR_ID}] as any[]};
+
+        // ONLY ALLOWS FOR NON LIASON VALUE SEARCHES
+        const {GA_CODEARTICLE, GA_LIBCOMPL, GA_LIBELLE} = req.query;
+
+        if(GA_CODEARTICLE) {
+            const regEx = new RegExp(GA_CODEARTICLE as string, "i");
+            filter.$and.push({GA_CODEARTICLE : {$regex: regEx}})
+        }
+        
+        if(GA_LIBCOMPL) {
+            const regEx = new RegExp(GA_LIBCOMPL as string, "i");
+            filter.$and.push({GA_LIBCOMPL : {$regex: regEx}})
+        }
+
+        if(GA_LIBELLE) {
+            const regEx = new RegExp(GA_LIBELLE as string, "i");
+            filter.$and.push({GA_LIBELLE : {$regex: regEx}})
+        }
+
+        const data = await DraftModel.find(filter).skip(skip).limit(intLimit);
+
+        const total = await DraftModel.countDocuments(filter);
+
+        res.status(200).json({data, total});
+
+
+    } catch(err) {
+        console.error(err);
+        res.status(400).json({})
+    }
+})
 
 router.get(DRAFT + "/:id", verifyToken, async (req: Request & { user?: {id: string}}, res: Response) => {
     try {
@@ -44,45 +96,46 @@ router.get(DRAFT + "/:id", verifyToken, async (req: Request & { user?: {id: stri
 })
 
 // This will just fetch all the drafts that are in the avc backend database
-router.get(DRAFT + "/creator-id/:creatorId", verifyToken, async (req: Request & { user?: {id: string}}, res: Response) => {
-    try {
-        const creatorId = req.params.creatorId;
+// DEPRECATED - use the /draft route for getting all the drafts for a user
+// router.get(DRAFT + "/creator-id/:creatorId", verifyToken, async (req: Request & { user?: {id: string}}, res: Response) => {
+//     try {
+//         const creatorId = req.params.creatorId;
 
-        if(!creatorId) {
-            throw new Error("Creator Id was falsy")
-        }
+//         if(!creatorId) {
+//             throw new Error("Creator Id was falsy")
+//         }
 
-        if(!req.user) {
-            throw new Error("User was not authenticated");
-        }
-        // Make sure authorization token matches
-        const idFromToken = req.user.id;
+//         if(!req.user) {
+//             throw new Error("User was not authenticated");
+//         }
+//         // Make sure authorization token matches
+//         const idFromToken = req.user.id;
 
-        if(creatorId !== idFromToken) {
-            throw new Error("Id passed from user does not match their authentication token");
-        }
+//         if(creatorId !== idFromToken) {
+//             throw new Error("Id passed from user does not match their authentication token");
+//         }
 
-        const filter = { CREATOR_ID: idFromToken}
+//         const filter = { CREATOR_ID: idFromToken}
 
-        const data = await DraftModel.find(filter);
+//         const data = await DraftModel.find(filter);
 
-        if(!data) {
-            throw new Error("Finding the Drafts returned falsy for some reason")
-        }
+//         if(!data) {
+//             throw new Error("Finding the Drafts returned falsy for some reason")
+//         }
 
-        const total = await DraftModel.countDocuments(filter)
+//         const total = await DraftModel.countDocuments(filter)
 
-        res.status(200).json({data, total});
-
-
-
-    } catch(err) {
-        console.error(req.originalUrl + ", msg: error : " + err)
-        res.status(400).json({})
-    }
+//         res.status(200).json({data, total});
 
 
-})
+
+//     } catch(err) {
+//         console.error(req.originalUrl + ", msg: error : " + err)
+//         res.status(400).json({})
+//     }
+
+
+// })
 
 
 // This will just fetch all the drafts that are in the avc backend database
