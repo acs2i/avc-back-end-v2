d22d818b105e11dcbd9d56f7e3063d98764a4271
Vincent Coccina <v.coccina@acs2i.fr>
2024-06-27 11:57:42 +0200

    fix

--- src/routes/draft/draftGet.ts
+++ src/routes/draft/draftGet.ts
@@ -137,4 +137,24 @@ router.get(DRAFT + "/user/:userId", verifyToken, async (req: Request, res: Respo
   }
 });
 
+
+router.get(DRAFT + "/draft/:id", verifyToken, async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  try {
+
+
+    const draft = await DraftModel.findById(id)
+    res.status(200).json(draft);
+  } catch (err) {
+    console.error("Error: ", err);
+    res
+      .status(500)
+      .json({
+        error:
+          "Une erreur est survenue lors de la récupération des brouillons.",
+      });
+  }
+});
+
 export default router;
