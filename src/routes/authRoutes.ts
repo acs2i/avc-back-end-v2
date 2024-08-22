6fde24dbd3d0149ebe913d070601f86fbab9398e
RamexDeltaXOO <walid.bko47@gmail.com>
2024-08-22 11:43:45 +0200

    route mail reset

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,34 +1,25 @@
-import express, {Request, Response, NextFunction} from "express";
+import express, { Request, Response, NextFunction } from "express";
 import User from "../models/UserModel";
 import HttpError from "../models/http-errors";
 import bcrypt from "bcrypt";
 import { verifyToken } from "../middleware/auth";
 import jwt from "jsonwebtoken";
+import crypto from "crypto";
+import nodemailer from "nodemailer";
 
 const router = express.Router();
 
-//REGISTER
-//@POST
-//api/v1/auth/register
+// REGISTER
+// @POST
+// api/v1/auth/register
 router.post("/register", async (req: Request, res: Response, next: NextFunction) => {
-  const {
-    username,
-    email,
-    password,
-    authorization,
-    comment,
-    additionalFields
-  } = req.body;
-
+  const { username, email, password, authorization, comment, additionalFields } = req.body;
 
   let existingUser;
   try {
     existingUser = await User.findOne({ email: email.toLowerCase() });
   } catch (err) {
-    const error = new HttpError(
-      "Signing up failed, please try again later.",
-      500
-    );
+    const error = new HttpError("Signing up failed, please try again later.", 500);
     return next(error);
   }
 
@@ -45,10 +36,7 @@ router.post("/register", async (req: Request, res: Response, next: NextFunction)
     try {
       additionalFieldsMap = new Map(additionalFields.map((field: { key: string; value: any }) => [field.key, field.value]));
     } catch (err) {
-      const error = new HttpError(
-        "Invalid format for additionalFields.",
-        400
-      );
+      const error = new HttpError("Invalid format for additionalFields.", 400);
       return next(error);
     }
   }
@@ -59,61 +47,142 @@ router.post("/register", async (req: Request, res: Response, next: NextFunction)
     email,
     authorization,
     comment,
-    additionalFields: additionalFieldsMap
+    additionalFields: additionalFieldsMap,
   });
 
   try {
     await newUser.save();
   } catch (err) {
-    console.error("Error saving user:", err);  // Log the error
-    const error = new HttpError(
-      "Echec lors de la création du compte, réessayez plus tard.",
-      500
-    );
+    console.error("Error saving user:", err);
+    const error = new HttpError("Echec lors de la création du compte, réessayez plus tard.", 500);
     return next(error);
   }
 
   res.status(200).json({ user: newUser.toObject({ getters: true }) });
 });
 
-//LOGIN
-//@POST
-//api/v1/auth/register
+// LOGIN
+// @POST
+// api/v1/auth/login
 router.post("/login", async (req: Request, res: Response, next: NextFunction) => {
   const { username, password } = req.body;
   let user;
+  console.log(username);
   try {
-    user = await User.findOne({ username: username });
-      if (!user) {
-        return res.status(404).json({ error: "Cet utilisateur n'existe pas." });
-      }
-    
-      const isMatch = await bcrypt.compare(password, user.password);
-      if (!isMatch) {
-        return res.status(404).json({ error: "L'identifiant et le mot de passe ne correspondent pas" });
-      }
-      if (!process.env.JWT_SECRET) {
-        throw new Error("JWT_SECRET non défini dans les variables d'environnement");
-      }
-      
-      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
-      
-      res.status(200).json({
-        message: "Logged In",
-        user: user.toObject({ getters: true }),
-        token: token
-      });
-  } catch (err) {
-    console.error("Error: " , err);
-    res.status(400).json({})
-  }
+    user = await User.findOne({ username });
+    if (!user) {
+      return res.status(404).json({ error: "Cet utilisateur n'existe pas." });
+    }
 
- 
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(404).json({ error: "L'identifiant et le mot de passe ne correspondent pas" });
+    }
+    if (!process.env.JWT_SECRET) {
+      throw new Error("JWT_SECRET non défini dans les variables d'environnement");
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+    res.status(200).json({
+      message: "Logged In",
+      user: user.toObject({ getters: true }),
+      token,
+    });
+  } catch (err) {
+    console.error("Error: ", err);
+    res.status(400).json({});
+  }
 });
 
-//LOGIN
-//@GET
-//api/v1/auth/register
+// FORGOT PASSWORD
+// @POST
+// api/v1/auth/forgot-password
+router.post("/forgot-password", async (req: Request, res: Response, next: NextFunction) => {
+  const { email } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ error: "Utilisateur non trouvé" });
+    }
+
+    // Générer un token de réinitialisation
+    const resetToken = crypto.randomBytes(32).toString("hex");
+    const resetTokenExpiry = Date.now() + 3600000; // 1 heure d'expiration
+
+    user.resetPasswordToken = resetToken;
+    user.resetPasswordExpires = new Date(resetTokenExpiry);;
+    await user.save();
+
+    // Configuration du transporteur d'email
+    const transporter = nodemailer.createTransport({
+      service: "Outlook365",
+      host: "acs2i-fr.mail.protection.outlook.com",
+      port: 587,
+      secure: false, 
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS, 
+      },
+      tls: {
+        rejectUnauthorized: false,
+      },
+    });
+
+    const resetURL = `${process.env.FRONTEND_URL}/reset-password?token=${resetToken}`;
+
+    const mailOptions = {
+      to: user.email,
+      from: process.env.EMAIL_USER,
+      subject: "Réinitialisation du mot de passe",
+      text: `Vous recevez cet e-mail car vous (ou quelqu'un d'autre) avez demandé la réinitialisation du mot de passe de votre compte.\n\n
+            Cliquez sur le lien suivant ou copiez-le dans votre navigateur pour compléter le processus :\n\n
+            ${resetURL}\n\n
+            Si vous n'avez pas demandé cela, veuillez ignorer cet e-mail et votre mot de passe restera inchangé.\n`,
+    };
+
+    await transporter.sendMail(mailOptions);
+
+    res.status(200).json({ message: "Un e-mail a été envoyé avec les instructions pour réinitialiser votre mot de passe." });
+  } catch (error) {
+    console.error("Erreur lors de l'envoi de l'e-mail de réinitialisation de mot de passe", error);
+    res.status(500).json({ error: "Une erreur est survenue." });
+  }
+});
+
+// RESET PASSWORD
+// @POST
+// api/v1/auth/reset-password
+router.post("/reset-password", async (req: Request, res: Response, next: NextFunction) => {
+  const { token, newPassword } = req.body;
+
+  try {
+    const user = await User.findOne({
+      resetPasswordToken: token,
+      resetPasswordExpires: { $gt: Date.now() }, // Remplacez `resetPasswordExpiry` par `resetPasswordExpires`
+    });
+    
+    if (!user) {
+      return res.status(400).json({ error: "Token invalide ou expiré" });
+    }
+    
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(newPassword, salt);
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpires = undefined; // Remplacez `resetPasswordExpiry` par `resetPasswordExpires`
+    await user.save();
+
+    res.status(200).json({ message: "Mot de passe réinitialisé avec succès" });
+  } catch (error) {
+    console.error("Erreur lors de la réinitialisation du mot de passe", error);
+    res.status(500).json({ error: "Une erreur est survenue." });
+  }
+});
+
+// GET ALL USERS
+// @GET
+// api/v1/auth/all-users
 router.get("/all-users", async (req: Request, res: Response, next: NextFunction) => {
   try {
     const users = await User.find();
@@ -124,10 +193,9 @@ router.get("/all-users", async (req: Request, res: Response, next: NextFunction)
   }
 });
 
-
-//Userbyid
-//@GET
-//api/v1/auth/user/:id
+// GET USER BY ID
+// @GET
+// api/v1/auth/user/:id
 router.get("/:userId", verifyToken, async (req: Request, res: Response, next: NextFunction) => {
   const { userId } = req.params;
   try {
@@ -139,19 +207,18 @@ router.get("/:userId", verifyToken, async (req: Request, res: Response, next: Ne
   }
 });
 
-//Notifications
-//@GET
-//api/v1/auth/user/:userId/notifications
+// GET USER NOTIFICATIONS
+// @GET
+// api/v1/auth/user/:userId/notifications
 router.get("/:userId/notifications", verifyToken, async (req: Request, res: Response, next: NextFunction) => {
   const { userId } = req.params;
-  const markAsRead = req.query.markAsRead === 'true';
   try {
-    const user = await User.findById(userId).select('notifications');
-    
+    const user = await User.findById(userId).select("notifications");
+
     if (!user) {
       return res.status(404).json({ error: "Utilisateur non trouvé" });
     }
-    
+
     res.status(200).json(user.notifications);
   } catch (err) {
     console.error("Error: ", err);
@@ -159,27 +226,27 @@ router.get("/:userId/notifications", verifyToken, async (req: Request, res: Resp
   }
 });
 
-//Mise a jour notifications
-//@PATCH
-//api/v1/auth/user/:userId/notifications
+// UPDATE USER NOTIFICATIONS
+// @PATCH
+// api/v1/auth/user/:userId/notifications
 router.patch("/:userId/notifications", verifyToken, async (req: Request, res: Response, next: NextFunction) => {
   const { userId } = req.params;
-  const markAsRead = req.query.markAsRead === 'true';
+  const markAsRead = req.query.markAsRead === "true";
 
   try {
-    const user = await User.findById(userId).select('notifications');
-    
+    const user = await User.findById(userId).select("notifications");
+
     if (!user) {
       return res.status(404).json({ error: "Utilisateur non trouvé" });
     }
 
     if (markAsRead) {
-      user.notifications.forEach(notification => {
+      user.notifications.forEach((notification) => {
         notification.read = true;
       });
       await user.save();
     }
-    
+
     res.status(200).json(user.notifications);
   } catch (err) {
     console.error("Error: ", err);
@@ -187,11 +254,9 @@ router.patch("/:userId/notifications", verifyToken, async (req: Request, res: Re
   }
 });
 
-
-
-//Update user
-//@GET
-//api/v1/auth/user/:id
+// UPDATE USER
+// @PATCH
+// api/v1/auth/user/:id
 router.patch("/:userId", verifyToken, async (req: Request, res: Response, next: NextFunction) => {
   const { userId } = req.params;
   const { username, authorization, email, password } = req.body;
@@ -216,12 +281,10 @@ router.patch("/:userId", verifyToken, async (req: Request, res: Response, next: 
     }
 
     if (password) {
-      // Hash the new password before saving it
       const salt = await bcrypt.genSalt(10);
       user.password = await bcrypt.hash(password, salt);
     }
 
-    // Sauvegarder les modifications
     const updatedUser = await user.save();
 
     res.status(200).json(updatedUser);
