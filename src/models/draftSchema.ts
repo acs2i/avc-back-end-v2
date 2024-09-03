8506adfc456baf9cb7ba633712def43c48c4e6e3
Vincent Coccina <v.coccina@acs2i.fr>
2024-09-03 13:59:47 +0200

    model draft

--- src/models/draftSchema.ts
+++ src/models/draftSchema.ts
@@ -2,35 +2,35 @@ import { ObjectId } from "mongodb";
 import mongoose, { Schema, Document, Model } from "mongoose";
 
 interface PriceItemSchema {
-    peau: number;       // Prix d'achat en unité
-    tbeu_pb: number;    // Taux de base en unité - prix de base
-    tbeu_pmeu: number;  // Taux de base en unité - prix modifié
-  }
+  peau: number; // Prix d'achat en unité
+  tbeu_pb: number; // Taux de base en unité - prix de base
+  tbeu_pmeu: number; // Taux de base en unité - prix modifié
+}
 
-  interface PriceSchema {
-    tarif_id: ObjectId;       // ID du tarif
-    currency: string;         // Devise
-    supplier_id: ObjectId;    // ID du fournisseur
-    price: PriceItemSchema[];   // Détail des prix
-    store: string;            // Magasin
-  }
+interface PriceSchema {
+  tarif_id: ObjectId; // ID du tarif
+  currency: string; // Devise
+  supplier_id: ObjectId; // ID du fournisseur
+  price: PriceItemSchema[]; // Détail des prix
+  store: string; // Magasin
+}
 
-  interface UvcSchema {
-    code: string;              // Code de l'UVC
-    dimensions: string[];      // Dimensions de l'UVC
-    prices: PriceSchema[];     // Liste des prix
-    eans: string[];            // Liste des codes EAN
-    status: number;            // Statut de l'UVC
-    additional_fields: any;    // Champs additionnels
-  }
+interface UvcSchema {
+  code: string; // Code de l'UVC
+  dimensions: string[]; // Dimensions de l'UVC
+  prices: PriceSchema[]; // Liste des prix
+  eans: string[]; // Liste des codes EAN
+  status: number; // Statut de l'UVC
+  additional_fields: any; // Champs additionnels
+}
 
-  interface SupplierSchema {
-    supplier_id: ObjectId;     // ID du fournisseur
-    supplier_ref: string;      // Référence du fournisseur
-    pcb: string;               // PCB
-    custom_cat: string;        // Catégorie douanière
-    made_in: string;           // Pays d'origine
-  }
+interface SupplierSchema {
+  supplier_id: ObjectId; // ID du fournisseur
+  supplier_ref: string; // Référence du fournisseur
+  pcb: string; // PCB
+  custom_cat: string; // Catégorie douanière
+  made_in: string; // Pays d'origine
+}
 
 interface DraftSchema extends Document {
   creator_id: ObjectId;
@@ -44,20 +44,20 @@ interface DraftSchema extends Document {
   dimension_types: string[];
   brand_ids: ObjectId[];
   collection_ids: ObjectId[];
+  peau: number;
+  tbeu_pb: number;
+  tbeu_pmeu: number;
   imgPath: string;
   status: string;
   additional_fields: any;
   uvc: UvcSchema[];
 }
 
-
-const priceItemSchema = new mongoose.Schema<PriceItemSchema> ({
-    peau:{ type: Number },      
-    tbeu_pb:{ type: Number },
-    tbeu_pmeu: { type: Number }
-}
-)
-
+const priceItemSchema = new mongoose.Schema<PriceItemSchema>({
+  peau: { type: Number },
+  tbeu_pb: { type: Number },
+  tbeu_pmeu: { type: Number },
+});
 
 const priceSchema = new mongoose.Schema<PriceSchema>(
   {
@@ -71,15 +71,15 @@ const priceSchema = new mongoose.Schema<PriceSchema>(
 );
 
 const supplierSchema = new mongoose.Schema<SupplierSchema>(
-    {
-      supplier_id: { type: mongoose.Schema.Types.ObjectId, ref: "supplier" },
-      supplier_ref: { type: String },
-      pcb: { type: String },
-      custom_cat: { type: String },
-      made_in: { type: String },
-    },
-    { _id: false }
-  );
+  {
+    supplier_id: { type: mongoose.Schema.Types.ObjectId, ref: "supplier" },
+    supplier_ref: { type: String },
+    pcb: { type: String },
+    custom_cat: { type: String },
+    made_in: { type: String },
+  },
+  { _id: false }
+);
 
 const uvcSchema = new mongoose.Schema<UvcSchema>(
   {
@@ -112,7 +112,12 @@ const draftSchema = new mongoose.Schema<DraftSchema>(
     suppliers: [supplierSchema],
     dimension_types: [{ type: String }],
     brand_ids: [{ type: mongoose.Schema.Types.ObjectId, ref: "brand" }],
-    collection_ids: [{ type: mongoose.Schema.Types.ObjectId, ref: "collection" }],
+    collection_ids: [
+      { type: mongoose.Schema.Types.ObjectId, ref: "collection" },
+    ],
+    peau: { type: Number },
+    tbeu_pb: { type: Number },
+    tbeu_pmeu: { type: Number },
     imgPath: { type: String },
     status: { type: String },
     additional_fields: {
@@ -124,5 +129,8 @@ const draftSchema = new mongoose.Schema<DraftSchema>(
   { timestamps: true, collection: "draft" }
 );
 
-const DraftModel: Model<DraftSchema> = mongoose.model<DraftSchema>("draft", draftSchema);
+const DraftModel: Model<DraftSchema> = mongoose.model<DraftSchema>(
+  "draft",
+  draftSchema
+);
 export default DraftModel;
