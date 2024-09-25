d73ee28e42a4f8116971bad9f66e5cbcd9e804a7
Jake Roberts <jdr81394@gmail.com>
2024-09-25 10:42:09 +0200

    draft post batch done

--- src/routes/draft/draftPost.ts
+++ src/routes/draft/draftPost.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from "express";
 import { DRAFT } from "./shared";
 import DraftModel from "../../models/draftSchema";
 import User from "../../models/UserModel";
-import { Document } from "mongoose";
+import { Document, Query } from "mongoose";
 import { Get } from "../../services/fetch";
 
 const router = express.Router();
@@ -186,34 +186,35 @@ router.post(DRAFT + "/import", async (req: Request, res: Response) => {
     }
 
 
-    let finalSupplierObj = []
+    // We will not be creating the object
+    // let finalSupplierObj = []
 
-    for(let i = 0; i < suppliers.length; i++) {
-      finalSupplierObj.push({ supplier_id: suppliers[i]._id})
-    }
+    // for(let i = 0; i < suppliers.length; i++) {
+    //   finalSupplierObj.push({ supplier_id: suppliers[i]._id})
+    // }
 
-    const draft = { collection_ids, brand_ids, tag_ids, reference , suppliers : finalSupplierObj} 
+    // const draft = { collection_ids, brand_ids, tag_ids, reference , suppliers : finalSupplierObj} 
 
-    const newDraft = await new DraftModel({ ...draft });
+    // const newDraft = await new DraftModel({ ...draft });
 
-    if (!newDraft) {
-      throw new Error(
-        req.originalUrl +
-          " msg: draft save did not work for some reason: " +
-          draft
-      );
-    }
+    // if (!newDraft) {
+    //   throw new Error(
+    //     req.originalUrl +
+    //       " msg: draft save did not work for some reason: " +
+    //       draft
+    //   );
+    // }
 
-    const result: Document | null | undefined = await newDraft.save({
-      timestamps: true,
-    });
-    if (creator_id) {
-      await User.findByIdAndUpdate(creator_id, {
-        $push: { products: newDraft._id },
-      });
-    }
+    // const result: Document | null | undefined = await newDraft.save({
+    //   timestamps: true,
+    // });
+    // if (creator_id) {
+    //   await User.findByIdAndUpdate(creator_id, {
+    //     $push: { products: newDraft._id },
+    //   });
+    // }
 
-    res.status(200).json(result);
+    res.status(200).json({});
   } catch (err) {
     console.error(err);
     res.status(400).json({});
@@ -261,6 +262,64 @@ router.post(DRAFT, async (req: Request, res: Response) => {
     }
 
     res.status(200).json(result);
+  } catch (err) {
+    console.error(err);
+    res.status(400).json({});
+  }
+});
+
+router.post(DRAFT + "/batch", async (req: Request, res: Response) => {
+  try  {
+    const drafts = req.body.drafts;
+    const userId = req.body.creator_id;
+
+    if (drafts.length === 0) {
+      throw new Error("req.body was empty");
+    }
+
+    if(!userId) {
+      throw new Error("No user id was present on the request")
+    }
+
+    // const id = req.body.id;
+    // const { long_label } = draft;
+
+    // const existingDraft = await DraftModel.findOne({ long_label, id });
+
+    // if (existingDraft) {
+    //   throw new Error(
+    //     req.originalUrl + ", msg: There already is a draft with this name"
+    //   );
+    // }
+
+    // const newDraft = await new DraftModel({ ...draft, id });
+
+    // if (!newDraft) {
+    //   throw new Error(
+    //     req.originalUrl +
+    //       " msg: draft save did not work for some reason: " +
+    //       draft
+    //   );
+    // }
+
+    // const result: Document | null | undefined = await newDraft.save({
+    //   timestamps: true,
+    // });
+    // if (userId) {
+    //   await User.findByIdAndUpdate(userId, {
+    //     $push: { products: newDraft._id },
+    //   });
+    // }
+
+    const insertedDrafts : any[] = await DraftModel.insertMany(drafts)
+    const draftIds = insertedDrafts.map((draft) => draft._id);
+
+    await User.findByIdAndUpdate(userId, {
+      $push: { products: draftIds},
+    });
+    
+
+    res.status(200).json(insertedDrafts);
   } catch (err) {
     console.error(err);
     res.status(400).json({});
